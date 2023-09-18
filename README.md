e36cd300e6e62fe292bd17eb2dbe8e1d20a2ec3b
Aaron Keesing <agkphysics@gmail.com>
2023-09-19 01:31:56 +1200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Show quote Tweets
+
+This simple Firefox add-on gives you a toolbar button to show quote
+tweets on Twitter/X.
---
+++ background.js
@@ -0,0 +1,12 @@
+browser.pageAction.onClicked.addListener((tab) => {
+    if (!/twitter\.com\/.*\/status/.test(tab.url))
+        return;
+    const newUrl = (
+        tab.url +
+        (tab.url.endsWith("/") ? "" : "/") +
+        "retweets/with_comments"
+    );
+    console.log(`Tab ${tab.id} -> ${newUrl}`);
+    browser.tabs.update(tab.id, { url: newUrl });
+});
+console.log("Added listeners for pageAction.");
