1bf719b4756ff3bffa484c4002d188b82e39441b
Aaron Keesing <agkphysics@gmail.com>
2023-11-29 01:18:09 +1300

    Fix case where query string is non-empty.

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 browser.pageAction.onClicked.addListener((tab) => {
     if (!/twitter\.com\/.*\/status/.test(tab.url))
         return;
-    const newUrl = (
-        tab.url +
-        (tab.url.endsWith("/") ? "" : "/") +
+    const newUrl = new URL(tab.url);
+    newUrl.search = "";
+    newUrl.hash = "";
+    newUrl.pathname = (
+        newUrl.pathname +
+        (newUrl.pathname.endsWith("/") ? "" : "/") +
         "retweets/with_comments"
     );
     console.log(`Tab ${tab.id} -> ${newUrl}`);
-    browser.tabs.create({ url: newUrl });
+    browser.tabs.create({ url: newUrl.href });
 });
 console.log("Added listeners for pageAction.");
